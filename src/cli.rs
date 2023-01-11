b9a6ba8112a2d9da46a52cae858d6a299f3b8ad5
Kleidi Bujari <mail@kleidi.ca>
2023-01-11 01:45:21 -0500

    tighten sortings, clean code

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{ArgAction::*, Parser, ValueEnum};
+use clap::{ArgAction::*, Parser, ValueEnum, ValueHint};
 
 use serde::Deserialize;
 use std::path::PathBuf;
@@ -10,11 +10,11 @@ pub const MIRROR_URL: &str = "https://archlinux.org/mirrors/status/json/";
 #[command(author, version)]
 pub struct Args {
     /// Custom URL used to fetch all mirrors
-    #[arg(short, long, value_name = "URL")]
+    #[arg(short, long, value_name = "URL", value_hint = ValueHint::Url)]
     pub url: Option<String>,
 
     /// Location to save mirrorlist to
-    #[arg(short, long, value_name = "FILE")]
+    #[arg(short, long, value_name = "FILE", value_hint = ValueHint::FilePath)]
     pub save: Option<PathBuf>,
 
     /// Number of mirrors to output to file
@@ -22,15 +22,21 @@ pub struct Args {
     pub number: Option<u16>,
 
     /// Protocols allowed to use
-    #[arg(short, long, value_name = "http|https|ftp", value_enum)]
+    #[arg(
+        short,
+        long,
+        value_name = "http|https|ftp",
+        value_enum,
+        required = true
+    )]
     pub protocol: Vec<Protocol>,
 
-    /// Restrict mirrors to a set of countries by name or code
-    #[arg(short, long, value_name = "Canada|CA", action = Append)]
+    /// Restrict mirrors to a set of countries or just one
+    #[arg(short, long, value_name = "France", action = Append, required = true)]
     pub country: Vec<String>,
 
     /// Sort filtered mirrors by a metric
-    #[arg(long, value_name = "age|rate|score")]
+    #[arg(long, value_name = "age|score", value_enum)]
     pub sort: Option<String>,
 
     /// Do not use mirrors that serve ISOs
@@ -66,4 +72,11 @@ impl Protocol {
             Self::Rsync => "rsync",
         }
     }
+}
+
+#[derive(Debug, ValueEnum, PartialEq, Eq, Clone, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Sort {
+    Age,
+    Score,
 }
