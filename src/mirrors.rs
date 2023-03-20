3fe2e5fbf3e89ea176ab41dc477a5931a58feca2
Kleidi Bujari <mail@kleidi.ca>
2023-03-20 17:46:03 -0400

    remove rustfmt.toml and reformat code

--- src/mirrors.rs
+++ src/mirrors.rs
@@ -2,40 +2,40 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct MirrorList {
-  pub cutoff: u32,
-  pub last_check: String,
-  pub num_checks: u8,
-  pub check_frequency: u16,
-  pub urls: Vec<Mirror>,
-  pub version: u8,
+    pub cutoff: u32,
+    pub last_check: String,
+    pub num_checks: u8,
+    pub check_frequency: u16,
+    pub urls: Vec<Mirror>,
+    pub version: u8,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Mirror {
-  pub url: String,
-  pub protocol: String,
-  pub last_sync: Option<String>,
-  pub completion_pct: f32,
-  pub delay: Option<i32>,
-  pub duration_avg: Option<f32>,
-  pub duration_stddev: Option<f32>,
-  pub score: Option<f64>,
-  pub active: bool,
-  pub country: String,
-  pub country_code: String,
-  pub isos: bool,
-  pub ipv4: bool,
-  pub ipv6: bool,
-  pub details: String,
+    pub url: String,
+    pub protocol: String,
+    pub last_sync: Option<String>,
+    pub completion_pct: f32,
+    pub delay: Option<i32>,
+    pub duration_avg: Option<f32>,
+    pub duration_stddev: Option<f32>,
+    pub score: Option<f64>,
+    pub active: bool,
+    pub country: String,
+    pub country_code: String,
+    pub isos: bool,
+    pub ipv4: bool,
+    pub ipv6: bool,
+    pub details: String,
 }
 
 impl std::fmt::Display for Mirror {
-  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-    write!(f, "Server = {}$repo/os/$arch", self.url)
-  }
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Server = {}$repo/os/$arch", self.url)
+    }
 }
 
 pub fn get_mirrors(url: &str) -> Result<MirrorList, reqwest::Error> {
-  let req: MirrorList = reqwest::blocking::get(url)?.json()?;
-  Ok(req)
+    let req: MirrorList = reqwest::blocking::get(url)?.json()?;
+    Ok(req)
 }
